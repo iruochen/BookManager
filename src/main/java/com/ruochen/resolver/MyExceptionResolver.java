649e3f6cf344a13b87c22edb1abcceed236d872d
ruochen <wangrui@ruochen.email>
2022-04-18 15:42:13 +0800

    异常处理

---
+++ src/main/java/com/ruochen/resolver/MyExceptionResolver.java
@@ -0,0 +1,17 @@
+package com.ruochen.resolver;
+
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyExceptionResolver implements HandlerExceptionResolver {
+
+    @Override
+    public ModelAndView resolveException(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("404");
+        return modelAndView;
+    }
+}
